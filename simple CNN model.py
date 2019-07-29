ff887a4b009d465125996a426c50e51a4cc7ed6a
Kinjal Dasgupta <kinjal.dasgupta.9@gmail.com>
2019-07-30 00:43:50 +0530

    Update simple CNN model.py

--- simple CNN model.py
+++ simple CNN model.py
@@ -12,9 +12,13 @@ batch_size = 128
 num_classes = 10
 epochs = 5
 img_rows, img_cols = 28, 28
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+(x_train, y_train), (x_test, y_test) =mnist.load_data()
 x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols,1)
 x_test = x_test.reshape(x_test.shape[0],  img_rows, img_cols,1)
+print('x_train shape:', x_train.shape)
+print('x test shape:',x_test.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
 input_shape = (img_rows,img_cols,1)
 x_train=x_train.astype('float32')
 x_test = x_test.astype('float32')
@@ -33,3 +37,4 @@ model.add(Dense(num_classes, activation='softmax'))
 model.compile(loss=keras.losses.categorical_crossentropy,optimizer='sgd',metrics=['accuracy'])
 model.fit(x_train, y_train,batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(x_test, y_test))
 score = model.evaluate(x_test, y_test, verbose=0)
+print('Test accuracy:', score)
