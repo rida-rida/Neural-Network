5fdba47422a05765467988b892e33b30758d9bfd
Kinjal Dasgupta <kinjal.dasgupta.9@gmail.com>
2020-04-21 19:41:24 +0530

    Update mnist_tf2.py

--- MNIST/mnist_tf2.py
+++ MNIST/mnist_tf2.py
@@ -16,7 +16,7 @@ mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
-# Add a channels dimension
+# Add a new channels dimension
 x_train = x_train[..., tf.newaxis]
 x_test = x_test[..., tf.newaxis]
 
